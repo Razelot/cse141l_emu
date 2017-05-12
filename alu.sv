c5e149710bc1bd8090c0b1f36be6adadbeee4291
Ezar Enanda <razelot@gmail.com>
2017-05-11 21:15:09 -0700

    remove black magic

--- alu.sv
+++ alu.sv
@@ -3,10 +3,10 @@
 module alu #(parameter reg_width = 8, parameter op_width = 3)
 (
   input signed [reg_width-1:0] ra_in,
-  input [reg_width-1:0] rb_in,
+  input signed [reg_width-1:0] rb_in,
   input [op_width-1:0] op,
-  output reg [reg_width-1:0] res_out,
-  output reg [reg_width-1:0] car_out,
+  output reg signed [reg_width-1:0] res_out,
+  output reg signed [reg_width-1:0] car_out,
   output logic zero,
   output logic jump
   );
@@ -26,149 +26,15 @@ module alu #(parameter reg_width = 8, parameter op_width = 3)
   end
   2: begin  // ADD
   res_out   <= ra_in +  rb_in;
-  car_out <= (127 <= ra_in + rb_in) ? 1'b1 : 1'b0;
+  car_out <= (128 > ra_in + rb_in) ? 1'b0 : 1'b1;
   end
   3: begin // SHIFT_RIGHT_LOGICAL
-  case (rb_in)
-    0 :begin
-    car_out <= 8'b00000000;
-    end
-    1:begin
-    res_out <= ra_in >> 1;
-    car_out <= ra_in << 7;
-    end
-    2: begin
-    res_out <= ra_in >> 2;
-    car_out <= ra_in << 6;
-    end
-    3: begin
-    res_out <= ra_in >> 3;
-    car_out <= ra_in << 5;
-    end
-    4: begin
-    res_out <= ra_in >> 4;
-    car_out <= ra_in << 4;
-    end
-    5: begin
-    res_out <= ra_in >> 5;
-    car_out <= ra_in << 3;
-    end
-    6: begin
-    res_out <= ra_in >> 6;
-    car_out <= ra_in << 2;
-    end
-    7: begin
-    res_out <= ra_in >> 7;
-    car_out <= ra_in << 1;
-    end
-    8: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in;
-    end
-    9: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in >> 1;
-    end
-    10: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in >> 2;
-    end
-    11: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in >> 3;
-    end
-    12: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in >> 4;
-    end
-    13: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in >> 5;
-    end
-    14: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in >> 6;
-    end
-    15: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in >> 7;
-    end
-    16: begin
-    res_out <= ra_in >> 8;
-    car_out <= ra_in >> 8;
-    end
-    endcase
-    end
+  res_out <= ra_in >> rb_in;
+  car_out <= (rb_in < 8) ? ra_in << (8 - rb_in) : ra_in >> (rb_in - 8);
+  end
   4: begin // SHIFT_RIGHT_ARITHMETIC
-  case (rb_in)
-    0 :begin
-    car_out <= 8'b00000000;
-    end
-    1:begin
-    res_out <= $signed(ra_in) >>> 1;
-    car_out <= ra_in << 7;
-    end
-    2: begin
-    res_out <= $signed(ra_in) >>> 2;
-    car_out <= ra_in << 6;
-    end
-    3: begin
-    res_out <= $signed(ra_in) >>> 3;
-    car_out <= ra_in << 5;
-    end
-    4: begin
-    res_out <= $signed(ra_in) >>> 4;
-    car_out <= ra_in << 4;
-    end
-    5: begin
-    res_out <= $signed(ra_in) >>> 5;
-    car_out <= ra_in << 3;
-    end
-    6: begin
-    res_out <= $signed(ra_in) >>> 6;
-    car_out <= ra_in << 2;
-    end
-    7: begin
-    res_out <= $signed(ra_in) >>> 7;
-    car_out <= $signed(ra_in) << 1;
-    end
-    8: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= ra_in;
-    end
-    9: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= $signed(ra_in) >>> 1;
-    end
-    10: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= $signed(ra_in) >>> 2;
-    end
-    11: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= $signed(ra_in) >>> 3;
-    end
-    12: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= $signed(ra_in) >>> 4;
-    end
-    13: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= $signed(ra_in) >>> 5;
-    end
-    14: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= $signed(ra_in) >>> 6;
-    end
-    15: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= $signed(ra_in) >>> 7;
-    end
-    16: begin
-    res_out <= $signed(ra_in) >>> 8;
-    car_out <= $signed(ra_in) >>> 8;
-    end
-    endcase
+  res_out <= ra_in >>> rb_in;
+  car_out <= (rb_in < 8) ? ra_in << (8 - rb_in) : ra_in >>> (rb_in - 8);
   end
   5: begin  // BEQ
   jump <= ra_in & rb_in;
