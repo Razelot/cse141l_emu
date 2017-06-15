9044ef7851351a252472b46dc265215b9ec3701d
Ezar Enanda <razelot@gmail.com>
2017-06-15 16:50:43 -0700

    branch bug

--- alu.sv
+++ alu.sv
@@ -23,26 +23,33 @@ module alu #(parameter reg_width = 8, op_width = 4)
   case(op)
   0: begin // AND
   res_out <= ra_in & rb_in;
+  branch <= 1'b0';
   end
   1: begin // SLT
   res_out <= $signed(ra_in) < $signed(rb_in) ? 0 : -1;
+  branch <= 1'b0';
   end
   2: begin // OR
   res_out <= ra_in | rb_in;
+  branch <= 1'b0';
   end
   3: begin // NOT
   res_out <= ~ra_in;
+  branch <= 1'b0';
   end
   4: begin // ADD
   res_out <= add[reg_width-1:0];
   car_out <= add[reg_width]; // OVERFLOW
+  branch <= 1'b0';
   end
   5: begin  // SUB
   res_out <= sub[reg_width-1:0];
   car_out <= sub[reg_width:reg_width-1] == 2'b10; // UNDERFLOW
+  branch <= 1'b0';
   end
   6: begin // SW, LW
   res_out <= ra_in;
+  branch <= 1'b0';
   end
   7: begin // BEQ
   branch <= ra_in == rb_in;
@@ -50,14 +57,17 @@ module alu #(parameter reg_width = 8, op_width = 4)
   8: begin // SHIFT_RIGHT_LOGICAL
   res_out <= ra_in >> rb_in;
   car_out <= (rb_in < 8) ? ra_in << (8 - rb_in) : ra_in >> (rb_in - 8);
+  branch <= 1'b0';
   end
   9: begin // SHIFT_RIGHT_ARITHMETIC
   res_out <= ra_in >>> rb_in;
   car_out <= (rb_in < 8) ? ra_in << (8 - rb_in) : ra_in >>> (rb_in - 8);
+  branch <= 1'b0';
   end
   10: begin // SHIFT_LEFT_LOGICAL
   res_out <= ra_in << rb_in;
   car_out <= (rb_in < 8) ? ra_in >> (8 - rb_in) : ra_in << (rb_in - 8);
+  branch <= 1'b0';
   end
   endcase
   end
