a42c1db47fa9a26e9284354be411ede947980298
Ezar Enanda <razelot@gmail.com>
2017-06-16 14:46:13 -0700

    small optimize

--- cordic_test/instr_mem.sv
+++ cordic_test/instr_mem.sv
@@ -12,27 +12,38 @@ module instr_mem #(parameter rom_size = 256, parameter instr_width = 9)
 
   assign instr_out = rom[instr_addr];
 
-  assign rom[0] = 'b001000110;
-  assign rom[1] = 'b001010100;
-  assign rom[2] = 'b101001000;
-  assign rom[3] = 'b001000110;
-  assign rom[4] = 'b001010100;
-  assign rom[5] = 'b101010000;
-  assign rom[6] = 'b001000110;
-  assign rom[7] = 'b001100100;
-  assign rom[8] = 'b001000110;
-  assign rom[9] = 'b001110100;
-  assign rom[10] = 'b000100001;
-  assign rom[11] = 'b000001111;
-  assign rom[12] = 'b110000101;
-  assign rom[13] = 'b001000110;
-  assign rom[14] = 'b001000110;
-  assign rom[15] = 'b001000110;
-  assign rom[16] = 'b001000110;
-  assign rom[17] = 'b111000000;
-
-
-
-
+  assign rom[0] = 'b001001110;
+  assign rom[1] = 'b001001110;
+  assign rom[2] = 'b001001110;
+  assign rom[3] = 'b001101100;
+  assign rom[4] = 'b001001110;
+  assign rom[5] = 'b001111100;
+  assign rom[6] = 'b011110000;
+  assign rom[7] = 'b101010110;
+  assign rom[8] = 'b001001110;
+  assign rom[9] = 'b001011100;
+  assign rom[10] = 'b101001000;
+  assign rom[11] = 'b001001110;
+  assign rom[12] = 'b001011100;
+  assign rom[13] = 'b101010000;
+  assign rom[14] = 'b000100001;
+  assign rom[15] = 'b000001111;
+  assign rom[16] = 'b110000101;
+  assign rom[17] = 'b001011011;
+  assign rom[18] = 'b001011111;
+  assign rom[19] = 'b001011110;
+  assign rom[20] = 'b101001111;
+  assign rom[21] = 'b010101011;
+  assign rom[22] = 'b101101110;
+  assign rom[23] = 'b111110100;
+  assign rom[24] = 'b111100100;
+  assign rom[25] = 'b101001111;
+  assign rom[26] = 'b000111010;
+  assign rom[27] = 'b101110110;
+  assign rom[28] = 'b001001010;
+  assign rom[29] = 'b001001010;
+  assign rom[30] = 'b001001010;
+  assign rom[31] = 'b001001010;
+  assign rom[32] = 'b111000000;
 
 endmodule
--- cordic_test/wave.do
+++ cordic_test/wave.do
@@ -1,6 +1,7 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
 add wave -noupdate /cordic_tb/DUT/DEC/instruction
+add wave -noupdate -radix unsigned /cordic_tb/DUT/IF/pc_out
 add wave -noupdate /cordic_tb/DUT/REG/car_in
 add wave -noupdate /cordic_tb/DUT/REG/core
 add wave -noupdate /cordic_tb/DUT/REG/rd_addr
@@ -10,9 +11,9 @@ add wave -noupdate /cordic_tb/DUT/REG/rs_addr
 add wave -noupdate /cordic_tb/DUT/REG/rs_out
 add wave -noupdate /cordic_tb/DUT/REG/rt_addr
 add wave -noupdate /cordic_tb/DUT/REG/rt_out
-add wave -noupdate -expand /cordic_tb/DUT/RAM/core
+add wave -noupdate /cordic_tb/DUT/RAM/core
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {118373 ps} 0}
+WaveRestoreCursors {{Cursor 1} {288547 ps} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 208
 configure wave -valuecolwidth 100
@@ -28,4 +29,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ps
 update
-WaveRestoreZoom {0 ps} {237692 ps}
+WaveRestoreZoom {7189843623 ps} {7189902968 ps}
